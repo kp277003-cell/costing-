71e75145283dcd3a0a963215d20b35fffe967072
kp277003-cell <kp277003@gmail.com>
2026-06-08 21:23:40 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = 'costing-v1';
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
